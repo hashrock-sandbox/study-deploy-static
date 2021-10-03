3cca1718d63e0c0b1d0762e6cb0e411a6fc034ae
hashrock <hashrock@users.noreply.github.com>
2021-10-03 15:39:13 +0900

    lint

--- mod.ts
+++ mod.ts
@@ -1,16 +1,16 @@
 async function handleRequest(request: Request) {
-    const { pathname } = new URL(request.url);
-    if (pathname.startsWith("/style.css")) {
-      const file = await Deno.readFile("./style.css");
-      return new Response(file, {
-        headers: {
-          "content-type": "text/css",
-        },
-      });
-    }
-  
-    return new Response(
-      `<html>
+  const { pathname } = new URL(request.url);
+  if (pathname.startsWith("/style.css")) {
+    const file = await Deno.readFile("./style.css");
+    return new Response(file, {
+      headers: {
+        "content-type": "text/css",
+      },
+    });
+  }
+
+  return new Response(
+    `<html>
         <head>
           <link rel="stylesheet" href="style.css" />
         </head>
@@ -18,14 +18,14 @@ async function handleRequest(request: Request) {
           <h1>Example</h1>
         </body>
       </html>`,
-      {
-        headers: {
-          "content-type": "text/html; charset=utf-8",
-        },
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
       },
-    );
-  }
-  
-  addEventListener("fetch", (event: FetchEvent) => {
-    event.respondWith(handleRequest(event.request));
-  });
+    }
+  );
+}
+
+addEventListener("fetch", (event: FetchEvent) => {
+  event.respondWith(handleRequest(event.request));
+});
